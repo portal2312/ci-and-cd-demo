092d8cc713d4a2bc88e074e690ec704f6260d1c4
portal2312 <portal2312@gmail.com>
2025-05-09 15:48:41 +0900

    Update actions-runnert

--- actions-runner/entrypoint.sh
+++ actions-runner/entrypoint.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-# NOTE: Docker Compose 에 의한 global volume 설정 된 경로의 권한을 변경합니다:
-# - Runner 스크립트를 실행할 수 없다
-# - build 단계에서 권할을 변경할 수 없어, container 시작 단계에서 권한 변경 한다
-sudo chown -R ${USERNAME}:${USERNAME} /actions-runner
-
 # 스크립트 실행 중 하나의 명령이라도 실패(즉, 종료 코드가 0이 아님)하면 즉시 스크립트 전체 실행을 중단하도록 만듭니다.
 set -e
 
-# # NOTE: configuration 에 필요한 환경 변수 확인합니다.
-if [ -z "$RUNNER_URL" ] || [ -z "$RUNNER_TOKEN" ] || [ -z "$RUNNER_NAME" ]; then
-  echo "[entrypoint] Check RUNNER_URL:${RUNNER_URL}, RUNNER_TOKEN:${RUNNER_TOKEN}, RUNNER_NAME:${RUNNER_NAME} environment variables"
+# NOTE: 환경 변수 확인하기
+if [ -z "$RUNNER_URL" ] || [ -z "$RUNNER_TOKEN" ] || [ -z "$RUNNER_NAME" ] || [ -z "$RUNNER_WORKDIR" ]; then
+  echo "[entrypoint] Check RUNNER_URL:${RUNNER_URL}, RUNNER_TOKEN:${RUNNER_TOKEN}, RUNNER_NAME:${RUNNER_NAME}, RUNNER_WORKDIR:${RUNNER_WORKDIR} environment variables"
   exit 1
 fi
 
-# NOTE: 이전에 configuration 했는 지 확인 후, 실행합니다.
 if [ ! -f ".runner" ]; then
   ./config.sh --url "$RUNNER_URL" \
     --token "${RUNNER_TOKEN}" \
